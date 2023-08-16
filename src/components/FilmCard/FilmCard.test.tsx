fe1829df10828cba7a2fddceab582cfa33c96348
Ana Lambea <analambeap@gmail.com>
2023-08-16 12:22:29 +0200

    Use filmMock object as a testing variable

--- src/components/FilmCard/FilmCard.test.tsx
+++ src/components/FilmCard/FilmCard.test.tsx
@@ -5,7 +5,7 @@ import FilmCard from "./FilmCard";
 describe("Given a FilmCard component", () => {
   describe("When it's rendered", () => {
     test("Then it should show the heading text 'Viaje a Darjeeling (2007)'", () => {
-      const headingText = "Viaje a Darjeeling (2007)";
+      const headingText = `${filmMock.title} (${filmMock.year})`;
 
       render(<FilmCard film={filmMock} />);
 
